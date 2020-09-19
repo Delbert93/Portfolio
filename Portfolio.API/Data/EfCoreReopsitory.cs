7eda21068c140e6f89a5ec24e1cbc6ecf221103c
McKinnin <lloydmckinnin@gmail.com>
2020-09-19 10:40:40 -0600

    Add project files.

---
+++ Portfolio.API/Data/EfCoreReopsitory.cs
@@ -0,0 +1,24 @@
+﻿using Portfolio.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.API.Data
+{
+    public class EfCoreReopsitory : IRepository
+    {
+        private readonly ApplicationDbContext context;
+        public EfCoreReopsitory(ApplicationDbContext context)
+        {
+            this.context = context ?? throw new ArgumentException(nameof(context));
+        }
+        public IQueryable<Project> Projects => throw new NotImplementedException();
+
+        public async Task SaveProjectAsync(Project project)
+        {
+            context.Projects.Add(project);
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Portfolio.API/Models/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.API.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Requirements { get; set; }
+        //TODO This might change to an image later.
+        public string Design { get; set; }
+
+        public DateTime CompletionDate { get; set; }
+    }
+}
